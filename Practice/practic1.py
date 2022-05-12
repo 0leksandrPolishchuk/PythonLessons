ae2335f53da5f22eca54bb048f450b1855db9b3a
Oleksandr Polishchuk <oleksandr.polishchuk@icloud>
2022-05-12 20:24:36 +0100

    If lessons

--- Practice/practic1.py
+++ Practice/practic1.py
@@ -15,29 +15,93 @@
 
 
 # DIALOGS WITH ROBOT PYTHON
-say_hello = "Robot:\tHello, my name is Python "
-print(say_hello)
-robot_question1 = "Robot:\tWhat is your name?"
-print(robot_question1)
-user_answer1 = "User:\t"
-robot_question = "\tHow old are you"
-print("Robot:", robot_question + ",", input(user_answer1) + "?")
-user_answer2 = int(input("User:\t"))
-current_year = "2022"
-robot_answer = "Robot:\tSo, you birt in:"
-print(robot_answer, int(current_year) - user_answer2, "?")
-user_answer3 = input("User:\t")
-robot_answer = "Robot:\tExcellent"
-print(robot_answer)
-robot_answer = "Robot:\tSo my calculation are correct :-)"
-print(robot_answer)
+# say_hello = "Robot:\tHello, my name is Python "
+# print(say_hello)
+# robot_question1 = "Robot:\tWhat is your name?"
+# print(robot_question1)
+# user_answer1 = "User:\t"
+# robot_question = "\tHow old are you"
+# print("Robot:", robot_question + ",", input(user_answer1) + "?")
+# user_answer2 = int(input("User:\t"))
+# current_year = "2022"
+# robot_answer = "Robot:\tSo, you birt in:"
+# print(robot_answer, int(current_year) - user_answer2, "?")
+# user_answer3 = input("User:\t")
+# robot_answer = "Robot:\tExcellent"
+# print(robot_answer)
+# robot_answer = "Robot:\tSo my calculation are correct :-)"
+# print(robot_answer)
+#
+# robot = "Robot:\tlet's play a little"
+# print(robot)
+# robot1 = "Robot:\tyou say, you:"
+# print(robot1, user_answer2)
+# robot2 = "Robot:\tlets calculate, enter any number below"
+# print(robot2)
+# user_answer4 = input("User:\t")
+# print("Robot:\t"+user_answer4, "-", user_answer2, "=",
+#       int(user_answer4) - int(user_answer2), ";)")
+# robot3 = "Robot:\tEnter the names of any countries " \
+#          "in the world, and I will tell you how many regions there are in " \
+#          "this country"
+# print(robot3)
 
-robot = "Robot:\tlet's play a little"
-print(robot)
-robot1 = "Robot:\tyou say, you:"
-print(robot1, user_answer2)
-robot2 = "Robot:\tlets calculate, enter any number below"
-print(robot2)
-user_answer4 = input("User:\t")
-print("Robot:\t"+user_answer4, "-", user_answer2, "=",
-      int(user_answer4) - int(user_answer2), ";)")
+# countries = input("User:\t")
+# if countries == "Ukraine":
+#     print("Robot:\t" + countries + ",", "Have 24 regions.")
+# elif countries == "Italy":
+#     print("Robot:\t" + countries + ",", "Have 20 regions.")
+# else:
+#     print("Robot:\tNo recognise you countries, please enter again"
+
+# a = ["USA", "UK", "Italy", "Ukraine"]
+#
+# q = input("Введіть країну: ")
+# if q in a:
+#     print(True)
+# else:
+#     print(False)
+
+# while True:
+#     countries = input("User:\t")
+#     if countries == "Ukraine":
+#         print("Robot:\t" + countries + ",", "Have 24 regions.")
+#         break
+#     elif countries == "Italy":
+#         print("Robot:\t" + countries + ",", "Have 20 regions.")
+#         break
+#     else:
+#         print("Robot:\tNo recognise you countries, please enter again")
+#         continue
+
+# n1 = input("1: ")
+# n2 = bool(input("2: "))
+# print("1", n1, type(n1))
+# print("2", n2, type(n2))
+
+
+# name = input("Enter you name: ")
+# last_name = input("Enter your last name: ")
+# age = int(input("Enter your year of birth: "))
+# is_married = bool(input("Enter you married: "))
+#
+# if (2022 - age) >= 18:
+#     print(name, last_name, "can get driver license")
+# else:
+#     print(name, last_name, "don't cant get drive license")
+
+
+a = int(input("Please enter minimum sleep hours: "))
+b = int(input("Please enter maximum sleep hours: "))
+h = int(input("Please enter how many hours you sleep: "))
+
+if h >= a and h <= b:
+    print("Good")
+elif h <= a:
+    print("No good")
+elif h >= b:
+    print("To moutch")
+
+
+
+
