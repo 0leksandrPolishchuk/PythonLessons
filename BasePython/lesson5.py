9174b992c3101bbce6c6fa6ef1fa0b4a0b5cef4f
Oleksandr Polishchuk <oleksandr.polishchuk@icloud>
2022-05-24 20:19:25 +0100

    Cykl - while

---
+++ BasePython/lesson5.py
@@ -0,0 +1,50 @@
+# (CYKLY) (ityracia cyklu)
+# while - Cykl umowy
+# for - Cykl progonu
+
+# counter = 0
+# while counter < 5:
+#     print("Oleksandr")
+#     counter += 1
+
+# number = 1
+# while number < 5:
+#     print("number:", number)
+#     number += 1
+
+# number = -10
+# while number <= 10:
+#     print("number:", number)
+#     number += 1
+
+# message = input("enter text: ")
+# counter = 0
+# while counter < 10:
+#     print(message)
+#     print(counter)
+#     counter += 1
+
+# counter = 0
+# while True:
+#     print(counter)
+#     counter += 1
+#     if counter > 100:
+#         break
+
+# while True:
+#     name = input("Please enter you name: ")
+#     if name == "Segiy":
+#         print("Cykl finito")
+#         break
+#     else:
+#         print(name)
+
+# while True:
+#     number = int(input("Please enter value: "))
+#     if number < 100:
+#         break
+#     elif number > 1000:
+#         print("Big number")
+#         continue
+#     else:
+#         print(number)
--- Practice/practic2.py
+++ Practice/practic2.py
@@ -1,13 +1,15 @@
-# CURRENCY EXCHANGE CALCULATOR
-print("CURRENCY EXCHANGE CALCULATOR")
-print("Available Only: UAH, Euro, USD, British Pound")
-currency_what = input("Enter what currency you want to exchange: ")
-currency_get = input("Enter what currency you want to get: ")
+# # CURRENCY EXCHANGE CALCULATOR
+# print("CURRENCY EXCHANGE CALCULATOR")
+# print("Available Only: UAH, Euro, USD, British Pound")
+# currency_what = input("Enter what currency you want to exchange: ")
+# currency_get = input("Enter what currency you want to get: ")
+#
+# print("Enter how many", currency_what, "you want exchange" + ": ", end=""),
+# currency1 = float(input())
+#
+# print("Enter value", currency_what, "to", currency_get + ": ", end=""),
+# currency2 = float(input())
+#
+# print("You get:", currency1 * currency2, currency_get)
 
-print("Enter how many", currency_what, "you want exchange" + ": ", end=""),
-currency1 = float(input())
 
-print("Enter value", currency_what, "to", currency_get + ": ", end=""),
-currency2 = float(input())
-
-print("You get:", currency1 * currency2, currency_get)
