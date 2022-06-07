ae7b36c1939808021e9d5dcf82ab0643ff9e54dd
Oleksandr Polishchuk <oleksandr.polishchuk@icloud>
2022-06-07 18:54:14 +0100

    practic list

--- BasePython/list_lesson.py
+++ BasePython/list_lesson.py
@@ -86,15 +86,15 @@
 #     print(i, "len:", len(i))
 
 
-peoples = ["Oleksandr", "Alan"]
-print(peoples)
-peoples.append("Serhii")  # Метод append - добавляє елемент в кінець листа.
-print(peoples)
-peoples.insert(1, "Alanik")  # Метод insert - добавляє елемент по індексу.
-print(peoples)
-peoples.extend([1, 2, 3])  # Метод extend - розширює  елементи в (list)
-print(peoples)
-print(peoples.index("Serhii"), peoples.index("Alanik"))  # Метод index - показує під яким індексом
+# peoples = ["Oleksandr", "Alan"]
+# print(peoples)
+# peoples.append("Serhii")  # Метод append - добавляє елемент в кінець листа.
+# print(peoples)
+# peoples.insert(1, "Alanik")  # Метод insert - добавляє елемент по індексу.
+# print(peoples)
+# peoples.extend([1, 2, 3])  # Метод extend - розширює  елементи в (list)
+# print(peoples)
+# print(peoples.index("Serhii"), peoples.index("Alanik"))  # Метод index - показує під яким індексом
                                                          # знаходиться певний елемент.
 # peoples.pop(3)  # Метод pop - видаляє елементи листа по індексу.
 # print(peoples)
@@ -102,3 +102,104 @@ print(peoples.index("Serhii"), peoples.index("Alanik"))  # Метод index - п
 # print(peoples)
 # peoples.clear()  # Метод clear - видаляє всі елементи з (list)
 # print(peoples)
+
+
+# user = [['a', 1], ['b', 2], ['b', 3]]
+# print(user)
+# print(user[0])
+# print(user[0][1])
+
+
+# user = {
+#     'Serhii': 123456,
+#     'Ahmed': 345234,
+#     'Oleg': 3241421
+# }
+#
+# print(user)
+#
+# while True:
+#     q = input("Please input user name:")
+#     pas = int(input("Please input user password: "))
+#     if pas == user.get(q):
+#         print('User', q, 'autorisate')
+#         break
+#     else:
+#         print('Error autorizations')
+#         print('Спробуйте ще раз!')
+
+
+# peoples = ["Alan", "Dima", "Vasia", "Diana"]
+# del peoples[0:2]
+# print(peoples)
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, ]
+# numbers = list(range(11))
+# print(numbers)
+# print(numbers[3:])
+# print(numbers[3:6])
+# print(numbers[:6])
+# print(numbers[::])
+# print(numbers[0:8:2])
+
+
+# for i in range(11):
+#     print(i)
+# for i in range(5, 11, 2):
+#     print(i)
+
+
+# peoples = ["Galia", "Serhii", "Valodia", "Taras", "Valodia"]
+# print(peoples.count("Valodia"))
+
+
+# numbers = [5, 50, 37, 80, 24, 45, 2, 100]
+# print(sorted(numbers))
+# print(numbers)
+# numbers.sort()
+# print(numbers)
+# numbers.sort(reverse=True)
+# print(numbers)
+
+# people = ["Tom",  "bob",  "alice",  "Sam",  "Bill"]
+# people.sort()
+# print(people)
+
+
+# people = []
+#
+# while True:
+#     name = input("Please input name: ").lower()
+#     if name == "stop":
+#         break
+#     else:
+#         people.append(name)
+#
+# print(people)
+# people.sort()
+# print(people)
+
+
+# numbers = [3, 67, 13, 45, 80, 20, 94, 22, 30]
+# print(numbers)
+# print(max(numbers))
+# print(min(numbers))
+
+
+# people1 = [45, 13, 10]
+# people2 = [20, 50, 90]
+# people3 = people1 + people2
+# print(people3)
+# people3.sort()
+# print(people3)
+
+# users = [
+#     ["Oleksandr", 37, 1985],
+#     ["Inna", 38],
+#     ["Emma", 9]
+# ]
+# print(users)
+# print(users[-1][0])
+# print(users[1][0])
+# print(users[0][2])
