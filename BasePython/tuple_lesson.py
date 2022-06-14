fdd94b5fa52c60442c3557ae3f331eb7d8c8b241
Oleksandr Polishchuk <oleksandr.polishchuk@icloud>
2022-06-14 20:11:16 +0100

    tuple lesson

---
+++ BasePython/tuple_lesson.py
@@ -0,0 +1,53 @@
+# Кортеж (tuple) - тип данних який не можна змінити.  Він працює швидше, тому його краще використовувати чим list, якщо
+# не потрібно нічого змінювати.
+
+
+# name = ("Nina", "Danik")
+# print(name)
+# print(type(name))
+
+
+# name = "Nina", "Danik"
+# print(name)
+# print(type(name))
+
+
+# name = ("Element",)
+# print(name)
+# print(type(name))
+
+
+# name = tuple()
+# print(name)
+# print(type(name))
+
+
+# name_list = ["Alan", "Vasia", "Dima", "Petro"]
+# print(name_list)
+# print(type(name_list))
+# name_tuple = tuple(name_list)
+# print(name_tuple)
+# print(type(name_tuple))
+
+
+# name_tuple = ("Dan", "Oleg", "John")
+# print(name_tuple)
+# print(type(name_tuple))
+# name_list = list(name_tuple)
+# print(name_list)
+
+
+# objects = ("Ala", "Simona", "Zachar", 1, 40, 2.5)
+# print(objects)
+# print(type(objects))
+# print(len(objects))
+
+
+# names = ("Pes", "Kit", 20, 50.0, "Dido", "Baba", "Car", "Town")
+# print(names[2])
+# print(names[-1])
+
+
+# name, age, company = ("Oleksandr", 37, "Lexus")
+# print(age)
+# print(company)
