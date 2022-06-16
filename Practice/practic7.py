b63ab3048ab8fb06586638618383196c19784c41
Oleksandr Polishchuk <oleksandr.polishchuk@icloud>
2022-06-16 20:14:59 +0100

    tuple practic

---
+++ Practice/practic7.py
@@ -0,0 +1,33 @@
+# name_list = ["Alex", 10, "Alan"]
+# name_tuple = tuple(name_list)
+# print(type(name_tuple))
+
+
+# name_tuple = ("Danik", "Emma", "Agnieszka")
+# name_list = list(name_tuple)
+# print(type(name_list))
+
+
+# cars = ("Ford", "Opel", "Audi", 5, 10, 25)
+# print(cars[2])
+# print(cars[-1])
+
+
+# name, surname, age = ("Alex", "Polishchuk", 37)
+# print(name)
+# print(surname)
+# print(age)
+
+
+# numbers = (10, 5.5, 20, "Alla", "Bigimot", 200, True, "Audi", [45, "Adam"], ("Ternopil", False))
+# print(numbers)
+# print(numbers[-2][1])
+# print(numbers[0:5])
+# print(numbers[5:8])
+# print(numbers[6:])
+# print(numbers[::2])
+#
+# i = 0
+# while i < len(numbers):
+#     print(numbers[i])
+#     i += 1
