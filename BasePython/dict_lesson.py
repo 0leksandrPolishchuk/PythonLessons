6e2b2d43bc4c049f3f4a016b0b750e27a9565248
Oleksandr Polishchuk <oleksandr.polishchuk@icloud>
2022-06-26 15:15:55 +0100

    Dict lesson

--- BasePython/dict_lesson.py
+++ BasePython/dict_lesson.py
@@ -65,12 +65,143 @@
 # }
 # print(name_dict)
 
-user = {
-    "Oleksandr": "Polishchuk",
-    "Agnieszka": "Knap",
-    "John": "Traworla",
-    "Olha": "Tverdochlib",
-    "Ivan": "Danyliv"
-}
-# print(user["Oleksandr"])
-print(user.get("John"))
+# user = {
+#     "Oleksandr": "Polishchuk",
+#     "Agnieszka": "Knap",
+#     "John": "Traworla",
+#     "Olha": "Tverdochlib",
+#     "Ivan": "Danyliv"
+# }
+# # print(user["Oleksandr"])
+# print(user.get("John"))
+
+# user = {
+#     "Dima": "Gromov",
+#     "Vasia": "Pupkin",
+#     "Sonia": "Sonkina"
+# }
+# del user["Vasia"] # del - це оператор.
+# print(user)
+
+# user = {
+#     "Dima": "Gromov",
+#     "Vasia": "Pupkin",
+#     "Sonia": "Sonkina"
+# }
+#
+# while True:
+#     user_check = input("enter user: ")
+#     if user_check in user:
+#         del user[user_check]
+#         print(user_check, "deleted")
+#     else:
+#         print(user_check, "not detected")
+#     print(user)
+
+# user = {
+#     "Dima": "Gromov",
+#     "Vasia": "Pupkin",
+#     "Sonia": "Sonkina"
+# }
+# user_check = input("enter user: ")
+# print(user.pop(user_check, "not detected"))
+# print(user)
+
+# cars = {
+#     "Fiat": "Doblo",
+#     "Ford": "Mustang",
+#     "BMW": "5 Series"
+# }
+# while True:
+#     cars_del = input("car delete enter 5: ")
+#     if cars_del == "5":
+#         cars.clear()
+#         print(cars, "deleted")
+#         break
+#     else:
+#         print("Error, try again")
+
+# city = {
+#     "Kiev": "Podol",
+#     "Lviv": "400000",
+#     "Ternopil": "Verbickogo"
+# }
+# print(city)
+# city2 = city.copy()
+# print(city2)
+
+# peoples = {
+#     "Alex": "age, 37",
+#     "Olia": "age, 50",
+#     "Oleh": "age, 30"
+# }
+# peoples_name = {
+#     "Oleksandr": "Polishchuk",
+#     "Agnieszka": "Polishchuk",
+#     "Alan": "Polishchuk"
+# }
+# peoples.update(peoples_name)
+# print(peoples)
+# print(peoples_name)
+
+# peoples = {
+#     "Alex": "age, 37",
+#     "Olia": "age, 50",
+#     "Oleh": "age, 30"
+# }
+# peoples_name = {
+#     "Oleksandr": "Polishchuk",
+#     "Agnieszka": "Polishchuk",
+#     "Alan": "Polishchuk"
+# }
+# all_peoples = {}
+# all_peoples.update(peoples)
+# all_peoples.update(peoples_name)
+# print(all_peoples)
+
+# years = {
+#     2021: "Alan birthday",
+#     1985: "Oleksandr birthday",
+#     1988: "Agnieszka birthday"
+# }
+# for i in years:
+#     print(i)
+
+# years = {
+#     2021: "Alan birthday",
+#     1985: "Oleksandr birthday",
+#     1988: "Agnieszka birthday"
+# }
+# for k, v in years.items():
+#     print(k, v)
+
+# years = {
+#     2021: "Alan birthday",
+#     1985: "Oleksandr birthday",
+#     1988: "Agnieszka birthday"
+# }
+# for i in years.values():
+#     print(i)
+
+# users = {
+#     "Oleksandr": {
+#         "phone": "28-77-48",
+#         "email": "oleksandr@icloud.com"
+#     },
+#     "Agnieszka": {
+#         "phone": "0753374599",
+#         "email": "agnieszka@gmail.com",
+#     },
+#     "Petro": {
+#         "phone": 7652347254,
+#         "email": "petro@gmail.com",
+#     }
+# }
+# print(users)
+# print(users["Oleksandr"])
+# print(users["Oleksandr"]["email"])
+#
+#
+# for i in users.values():
+#     for j in i.values():
+#         print(j)
--- Practice/practic6.py
+++ Practice/practic6.py
@@ -1,121 +1,121 @@
-print("<USER AND PASSWORD VERIFICATION>")
-
-users_pass = [["Oleksandr", "Alex1985"], ["Alan", "AlanAlan"],
-              ["Agnieszka", "1988"], ["Emma", "2013"], ["Daniel", "2010"]]
-test = False
-
-# Тут Провіряємо логін та пароль в сприскую
-while True:
-    login = input(" enter login: ")
-    password = input("enter password: ")
-
-    if login in users_pass[0] and password in users_pass[0]:
-        print("in list")
-    elif login in users_pass[1] and password in users_pass[1]:
-        print("in list")
-    elif login in users_pass[2] and password in users_pass[2]:
-        print("in list")
-    elif login in users_pass[3] and password in users_pass[3]:
-        print("in list")
-    elif login in users_pass[4] and password in users_pass[4]:
-        print("in list")
-        break
-    else:
-        print("Error! What you want change, enter l: <login> or  p: <password>")
-        break
-
-# Тут вибираємо що будем змінювати.
-if not test:
-    while True:
-        user_or_pass = input("enter option please: ")
-
-        if user_or_pass == "p":
-            print("Password selected")
-            test2 = True
-            break
-        elif user_or_pass == "l":
-            print("Login selected")
-            test1 = True
-            break
-        else:
-            print("Error! ENTER AGAIN")
-
-test = False
-
-# Тут Змінюємо пароль.
-if not test:
-    while True:
-        user_login = input("enter user login, witch  you need change password: ")
-
-        if user_login in users_pass[0]:
-            change_pass = input("enter new password: ")
-
-            users_pass[0][1] = change_pass
-            print(users_pass)
-        elif user_login in users_pass[1]:
-            change_pass = input("enter new password: ")
-
-            users_pass[1][1] = change_pass
-            print(users_pass)
-        elif user_login in users_pass[2]:
-            change_pass = input("enter new password: ")
-
-            users_pass[2][1] = change_pass
-            print(users_pass)
-        elif user_login in users_pass[3]:
-            change_pass = input("enter new password: ")
-
-            users_pass[3][1] = change_pass
-            print(users_pass)
-        elif user_login in users_pass[4]:
-            change_pass = input("enter new password: ")
-
-            users_pass[4][1] = change_pass
-            print(users_pass)
-            break
-        else:
-            user_login = "Stop"
-            print("Done, Password Changed")
-
-
-test = False
-
-# Тут змінюємо логін.
-if not test:
-    while True:
-        user_login = input("enter login, witch you need change : ")
-
-        if user_login in users_pass[0]:
-            change_login = input("enter new login: ")
-
-            users_pass[0][0] = change_login
-            print(users_pass)
-        elif user_login in users_pass[1]:
-            change_pass = input("enter new login: ")
-
-            users_pass[1][0] = change_pass
-            print(users_pass)
-        elif user_login in users_pass[2]:
-            change_login = input("enter new password: ")
-
-            users_pass[2][0] = change_login
-            print(users_pass)
-        elif user_login in users_pass[3]:
-            change_pass = input("enter new login: ")
-
-            users_pass[3][0] = change_pass
-            print(users_pass)
-        elif user_login in users_pass[4]:
-            change_pass = input("enter new login: ")
-
-            users_pass[4][0] = change_pass
-            print(users_pass)
-            break
-        else:
-            user_login = "Stop"
-            print("Done, Password Changed")
-            break
-
+# print("<USER AND PASSWORD VERIFICATION>")
+#
+# users_pass = [["Oleksandr", "Alex1985"], ["Alan", "AlanAlan"],
+#               ["Agnieszka", "1988"], ["Emma", "2013"], ["Daniel", "2010"]]
+# test = False
+#
+# # Тут Провіряємо логін та пароль в сприскую
+# while True:
+#     login = input(" enter login: ")
+#     password = input("enter password: ")
+#
+#     if login in users_pass[0] and password in users_pass[0]:
+#         print("in list")
+#     elif login in users_pass[1] and password in users_pass[1]:
+#         print("in list")
+#     elif login in users_pass[2] and password in users_pass[2]:
+#         print("in list")
+#     elif login in users_pass[3] and password in users_pass[3]:
+#         print("in list")
+#     elif login in users_pass[4] and password in users_pass[4]:
+#         print("in list")
+#         break
+#     else:
+#         print("Error! What you want change, enter l: <login> or  p: <password>")
+#         break
+#
+# # Тут вибираємо що будем змінювати.
+# if not test:
+#     while True:
+#         user_or_pass = input("enter option please: ")
+#
+#         if user_or_pass == "p":
+#             print("Password selected")
+#             test2 = True
+#             break
+#         elif user_or_pass == "l":
+#             print("Login selected")
+#             test1 = True
+#             break
+#         else:
+#             print("Error! ENTER AGAIN")
+#
+# test = False
+#
+# # Тут Змінюємо пароль.
+# if not test:
+#     while True:
+#         user_login = input("enter user login, witch  you need change password: ")
+#
+#         if user_login in users_pass[0]:
+#             change_pass = input("enter new password: ")
+#
+#             users_pass[0][1] = change_pass
+#             print(users_pass)
+#         elif user_login in users_pass[1]:
+#             change_pass = input("enter new password: ")
+#
+#             users_pass[1][1] = change_pass
+#             print(users_pass)
+#         elif user_login in users_pass[2]:
+#             change_pass = input("enter new password: ")
+#
+#             users_pass[2][1] = change_pass
+#             print(users_pass)
+#         elif user_login in users_pass[3]:
+#             change_pass = input("enter new password: ")
+#
+#             users_pass[3][1] = change_pass
+#             print(users_pass)
+#         elif user_login in users_pass[4]:
+#             change_pass = input("enter new password: ")
+#
+#             users_pass[4][1] = change_pass
+#             print(users_pass)
+#             break
+#         else:
+#             user_login = "Stop"
+#             print("Done, Password Changed")
+#
+#
+# test = False
+#
+# # Тут змінюємо логін.
+# if not test:
+#     while True:
+#         user_login = input("enter login, witch you need change : ")
+#
+#         if user_login in users_pass[0]:
+#             change_login = input("enter new login: ")
+#
+#             users_pass[0][0] = change_login
+#             print(users_pass)
+#         elif user_login in users_pass[1]:
+#             change_pass = input("enter new login: ")
+#
+#             users_pass[1][0] = change_pass
+#             print(users_pass)
+#         elif user_login in users_pass[2]:
+#             change_login = input("enter new password: ")
+#
+#             users_pass[2][0] = change_login
+#             print(users_pass)
+#         elif user_login in users_pass[3]:
+#             change_pass = input("enter new login: ")
+#
+#             users_pass[3][0] = change_pass
+#             print(users_pass)
+#         elif user_login in users_pass[4]:
+#             change_pass = input("enter new login: ")
+#
+#             users_pass[4][0] = change_pass
+#             print(users_pass)
+#             break
+#         else:
+#             user_login = "Stop"
+#             print("Done, Password Changed")
+#             break
+#
 
 # users_pass = [["Oleksandr", "Alex1985"], ["Alan", "AlanAlan"],
 #               ["Agnieszka", "1988"], ["Emma", "2013"], ["Daniel", "2010"]]
