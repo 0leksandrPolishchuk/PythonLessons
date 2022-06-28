a245c0422fbd425a52c27fdd75262cdb7902b33b
Oleksandr Polishchuk <oleksandr.polishchuk@icloud>
2022-06-28 18:53:05 +0100

    set lesson

---
+++ BasePython/set_lesson.py
@@ -0,0 +1,63 @@
+# users = {"Ivan", "Galina", "Petro", "Ivan", "Zosia"}
+# print(users)
+
+
+# peoples = ["Volodia", "Oksana", "Nazar", "Inna", "Nazar", "Volodia"]
+# set_peoples = set(peoples)
+# print(set_peoples)
+
+
+# users = set()
+# print(users)
+
+
+# cars = {"Fiat", "Lambo", "Passat", "Opel", "Audi", "BMW"}
+# print(len(cars))
+
+
+# object_set = {"Valodia", True, 10.5, 100, "London", False, 2, "Kiev", "London2022"}
+# print(object_set)
+
+
+# city = {"Kiev", "Lviv", "Odessa", "Ternopil"}
+# print(city)
+# city.add("Charkiv")
+# print(city)
+# city.remove("Lviv")
+# print(city)
+
+
+# city = {"Kiev", "Lviv", "Odessa", "Ternopil"}
+# a = input("enter city to delete: ")
+# if a in city:
+#     city.remove(a)
+# else:
+#     print("Не знайдено")
+# print(city)
+
+
+# city = {"Kiev", "Lviv", "Odessa", "Ternopil"}
+# a = input("enter city to delete: ")
+# city.discard(a)
+# print(city)
+
+
+# city = {"Kiev", "Lviv", "Odessa", "Ternopil"}
+# city.clear()
+# print(city)
+
+
+# device = {"Laptop", "Mouse", "Smartphone", "Keyboard"}
+# for i in device:
+#     print(i)
+
+# users = {"Admin", "User", "Manager"}
+# print(users)
+# student = users.copy()
+# print(student)
+
+
+# user1 = {"Kolia", "Galina", "Ivan"}
+# user2 = {"Valodia", "Dimon", "Olia"}
+# user3 = user1.union(user2)
+# print(user3)
